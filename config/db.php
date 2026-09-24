526d1b4b39edd1b05f83f0db52b81f74124527ec
Sindhuja GA <sindhuja371982@gmail.com>
2026-09-24 18:37:51 +0530

    Create db.php

---
+++ config/db.php
@@ -0,0 +1,21 @@
+<?php
+
+$host = "YOUR_DATABASE_HOST";
+$user = "YOUR_DATABASE_USERNAME";
+$password = "YOUR_DATABASE_PASSWORD";
+$database = "YOUR_DATABASE_NAME";
+
+$conn = new mysqli(
+    $host,
+    $user,
+    $password,
+    $database
+);
+
+if ($conn->connect_error) {
+    die("Database connection failed");
+}
+
+$conn->set_charset("utf8mb4");
+
+?>
